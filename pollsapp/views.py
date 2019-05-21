732e0ba16d79ef297f639b0ef917b9ee58ff9e25
queenfiona <fionaitya@gmail.com>
2019-05-21 23:11:05 +0300

    simplify the detail view

--- pollsapp/views.py
+++ pollsapp/views.py
@@ -1,7 +1,8 @@
 """pollsapp views."""
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 
 def index(request):
@@ -21,10 +22,22 @@ def detail(request, question_id):
 
 def results(request, question_id):
     """Display results for a particular question."""
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
     """Handle voting for a particular question."""
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['Choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('pollsapp:results',
+                                            args=(question.id,)))
