de864a9027b77364eeeaeba334f45c717b3da965
queenfiona <fionaitya@gmail.com>
2019-05-23 10:15:50 +0300

    customize admin interface

--- pollsapp/admin.py
+++ pollsapp/admin.py
@@ -2,5 +2,12 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    """docstring for QuestionAdmin."""
+
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
