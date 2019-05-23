cfd0a8eba2056eb672c025f6c85a8bdedf34cb58
queenfiona <fionaitya@gmail.com>
2019-05-23 10:41:18 +0300

    add choice object to question admin page

--- pollsapp/admin.py
+++ pollsapp/admin.py
@@ -3,16 +3,24 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    """docstring for ChoiceInline."""
+
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     """docstring for QuestionAdmin."""
 
     # The first element of each tuple in fieldsets is the title of the fieldset
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': [
+         'pub_date'], 'classes': ['collapse']}),
 
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
